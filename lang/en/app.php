298c6d186e44d945fd13b60fffc7cfb78446d5d2
cloudmyn <cloudmyn46@gmail.com>
2024-11-22 04:24:53 +0800

    first commit

---
+++ lang/en/app.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'welcome'       =>  'Welcome to :app_name.',
+    'simple page'   =>  'Simple Page',
+    'settings'      =>  'Settings',
+    'Env Editor'    =>  'Env Editor',
+];
---
+++ lang/id/app.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'welcome'       =>  'Selamat datang di :app_name.',
+    'simple page'   =>  'Halaman sederhana',
+    'settings'      =>  'Pengaturan',
+    'Env Editor'    =>  'Editor Env',
+];
